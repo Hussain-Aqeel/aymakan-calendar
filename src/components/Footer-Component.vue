f8d4b2fb40e95e4940b4546c560321c119451950
Hussain-Aqeel <hussain@ljassim.tech>
2022-06-15 09:17:55 +0300

    first commit

---
+++ src/components/Footer-Component.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer
+      class="flex bg-red-900 font-bold tracking-widest text-white p-7 text-xs md:text-lg mt-10">
+    <p>All rights reserved &copy; AyMakan Logistics 2022</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar-Component.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav
+      class="flex items-center mb-10 bg-red-900 font-bold tracking-widest text-white p-7 text-center md:text-xl">
+    <img src="../assets/logo.png"
+         alt="logo"
+         srcset=""
+         height="50"
+         width="50">
+    <h4 id="title"
+        class="ml-5">AyMakan Meeting Room Reserver</h4>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&display=swap');
+
+  #title {
+    font-family: 'Baloo 2', cursive;
+  }
+</style>
